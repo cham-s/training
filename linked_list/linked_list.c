c35f05751c29eeaf639722c5ed830c78ec548916
cham-s <cc.attou@gmail.com>
2019-08-10 20:58:12 +0200

    deleting node

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -30,6 +30,11 @@ void	add_node(t_node **list, int value)
 
 void	print_list(t_node **list)
 {
+	if (*list == NULL)
+	{
+		printf("(empty)");
+		return;
+	}
 	t_node *tmp = *list;
 	int passed = 0;
 	if (!passed)
@@ -48,14 +53,61 @@ void	print_list(t_node **list)
 	}
 }
 
+void	delete_node(t_node **list, int index)
+{
+	t_node	*tmp;
+	if (*list == NULL)
+		return;
+	if ((*list)->next == NULL && index == 0)
+	{
+		tmp = *list;
+		*list = NULL;
+		free(tmp);
+		return;
+	}
+	t_node	*slow = *list;
+	t_node	*fast = (*list)->next;
+	int		fast_index = 1;
+	while (fast != NULL)
+	{
+		if (index == 0)
+		{
+			tmp = slow;
+			*list = fast;
+			free(tmp);
+			return;
+		}
+		if (fast_index == index)
+		{
+			tmp = fast;
+			slow->next = fast->next;
+			free(tmp);
+			return;
+		}
+		slow = slow->next;
+		fast = fast->next;
+		fast_index += 1;
+	}
+}
+
 
 int main()
 {
 	t_node *list = NULL;
-	add_node(&list, 5);
-	add_node(&list, 5);
-	add_node(&list, 5);
-	add_node(&list, 5);
-	add_node(&list, 5);
+	add_node(&list, 0);
+	add_node(&list, 1);
+	add_node(&list, 2);
+	add_node(&list, 3);
+	add_node(&list, 4);
+	print_list(&list);
+	delete_node(&list, 0);
+	print_list(&list);
+	delete_node(&list, 0);
+	print_list(&list);
+	delete_node(&list, 0);
+	print_list(&list);
+	delete_node(&list, 0);
+	print_list(&list);
+	delete_node(&list, 0);
 	print_list(&list);
 }
