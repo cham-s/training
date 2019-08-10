7eb65f4cab9e1dd76ed72785cc18e542c8004bd8
cham-s <cc.attou@gmail.com>
2019-08-10 18:26:16 +0200

    hanlding min and max

--- piscine/j02/ft_putnbr.c
+++ piscine/j02/ft_putnbr.c
@@ -8,10 +8,14 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int n)
 {
+	int is_min = n == -2147483648;
 	if (n < 0)
 	{
 		ft_putchar('-');
-		n = -n;
+		if (is_min)
+			n = 214748364;
+		else
+			n = -n;
 	}
 	if (n <= ('9' - '0'))
 		ft_putchar('0' + n);
@@ -20,15 +24,17 @@ void	ft_putnbr(int n)
 		ft_putnbr(n/10);
 		ft_putnbr(n%10);
 	}
+	if (is_min)
+		ft_putchar('8');
 }
 
 int		main()
 {
-	ft_putnbr(4);
+	ft_putnbr(-2147483648);
 	ft_putchar('\n');
-	ft_putnbr(13);
+	ft_putnbr(2147483647);
 	ft_putchar('\n');
-	ft_putnbr(-133);
+	ft_putnbr(45);
 	ft_putchar('\n');
 	return (0);
 }
