8486e0b7b7c7fb934eec5a72cc968d685ff75914
cham-s <cc.attou@gmail.com>
2019-08-11 17:31:48 +0200

    added ft_putstr

---
+++ piscine/j03/ft_putstr.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str != '\0')
+	{
+		ft_putchar(*str);
+		str += 1;
+	}
+}
+
+int main()
+{
+	ft_putstr("Hello, world!");
+	return (0);
+}
